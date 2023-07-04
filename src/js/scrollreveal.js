0d8f9ce5972d34e54bfa667d6d1e2704b3769935
frans <franswinata6@gmail.com>
2023-07-04 13:30:05 +0700

    max width desktop

---
+++ src/js/scrollreveal.js
@@ -0,0 +1,17 @@
+ScrollReveal().reveal('.about-first', {
+  interval: 300,
+  reset: true,
+  delay: 500,
+  origin: 'bottom',
+  duration: 1000,
+  distance: '80px',
+});
+
+ScrollReveal().reveal('.why-us', {
+  interval: 300,
+  reset: true,
+  delay: 500,
+  origin: 'bottom',
+  duration: 1000,
+  distance: '80px',
+});
