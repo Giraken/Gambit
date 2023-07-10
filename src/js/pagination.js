58eddcf20f1fe882c2c4235b799372f4348dc9d0
Giraken <89152100+Giraken@users.noreply.github.com>
2023-07-10 19:37:47 +0700

    - About You page developing process

---
+++ src/js/pagination.js
@@ -0,0 +1,31 @@
+let link = document.getElementById("link");
+
+let currentValue = 1;
+
+function activeLink() {
+    for(l of link){
+        l.classList.remove("active");
+    }
+    event.target.classList.add("active");
+    currentValue = event.target.value;
+}
+
+function backBtn() {
+    if (currentValue > 1) {
+        for(l of link){
+            l.classList.remove("active");
+        }
+        currentValue--;
+        link[currentValue-1].classList.add("active");
+    }
+}
+
+function nextBtn() {
+    if (currentValue < 10) {
+        for(l of link){
+            l.classList.remove("active");
+        }
+        currentValue++;
+        link[currentValue-1].classList.add("active");
+    }
+}
